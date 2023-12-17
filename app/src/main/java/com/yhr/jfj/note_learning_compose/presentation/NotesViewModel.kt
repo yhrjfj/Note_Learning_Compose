2c53eaafa71af8c466fc8fd324d939f4439d3e18
RiJo <102893295+yhrjfj@users.noreply.github.com>
2023-12-17 18:03:08 +0600

    Start from NotesViewModel

--- app/src/main/java/com/yhr/jfj/note_learning_compose/presentation/NotesViewModel.kt
+++ app/src/main/java/com/yhr/jfj/note_learning_compose/presentation/NotesViewModel.kt
@@ -1,9 +1,29 @@
 package com.yhr.jfj.note_learning_compose.presentation
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.yhr.jfj.note_learning_compose.data.NoteDao
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
 
 class NotesViewModel(
     private val dao: NoteDao
 ) : ViewModel() {
+    private var isShortedByDateAdded = MutableStateFlow(true)
+
+    fun onEvent(event: NotesEvent){
+        when(event){
+            is NotesEvent.DeleteNote -> {
+                viewModelScope.launch {
+                    dao.deleteNote(event.note)
+                }
+            }
+            is NotesEvent.SaveNote -> {
+                TODO()
+            }
+            NotesEvent.SortNotes -> {
+                isShortedByDateAdded.value = !isShortedByDateAdded.value
+            }
+        }
+    }
 }
